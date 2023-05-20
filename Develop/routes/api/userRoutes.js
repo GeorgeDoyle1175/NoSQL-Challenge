b25b54a38d8163eb9a1a23e97741b50e1ee5d148
GeorgeDoyle1175 <119374026+GeorgeDoyle1175@users.noreply.github.com>
2023-05-20 12:00:36 -0400

    Update userRoutes.js

--- Develop/routes/api/userRoutes.js
+++ Develop/routes/api/userRoutes.js
@@ -5,14 +5,23 @@ const {
   createUser,
   updateUser,
   removeUser,
+  addFriend,
+  removeFriend,
 } = require('../../controllers/userController');
 
 // Define user routes
-router.route('/').get(getAllUsers).post(createUser);
+router.route('/')
+  .get(getAllUsers)
+  .post(createUser);
 
 router.route('/:userId')
   .get(getSingleUser)
   .put(updateUser)
   .delete(removeUser);
 
+router.route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend);
+
 module.exports = router;
+
